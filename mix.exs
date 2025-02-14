e24728f72fb0bb2421b2e2cfedf37a0930958326
DMPMS <monteiro_davi@yahoo.com>
2025-02-14 17:45:29 -0300

    Atualizando versao do guardian

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule TwitterClone.MixProject do
       {:gettext, "~> 0.11"},
       {:tailwind, "~> 0.2", runtime: Mix.env() == :dev},
       {:jason, "~> 1.0"},
-      {:guardian, "~> 2.0"},
+      {:guardian, "~> 2.2.1"},
       {:plug_cowboy, "~> 2.0"},
       {:comeonin, "~> 5.3.3"},
       {:bcrypt_elixir, "~> 2.3.1"}
